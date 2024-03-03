a79ba70fdd9e57fc6501b8934b4ed6b7b2064dc7
westin <westinp2009@gmail.com>
2024-03-03 17:53:40 -0500

    project structor started

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,19 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, fart])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn fart() -> String {
+    "fart ={ } ".to_string()
+}
---
+++ src/lib/greet.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greeting = '';
+	let fart = '';
+	/**
+     using tauri invoke to call the greet function
+	 notice how we are using tauri like an API to call the greet function
+	 * hints the async structure of the function
+	 NEET !
+     * */
+	async function greet() {
+		greeting = await invoke('greet', { name });
+	}
+
+	async function fartted() {
+		fart = await invoke('fart');
+		fartGone();
+	}
+
+	function fartGone() {
+		setTimeout(() => {
+			fart = '';
+		}, 2000);
+	}
+</script>
+
+<div>
+	<input bind:value={name} />
+	<button on:click={greet}>Greet</button>
+	<p>{greeting}</p>
+	<button on:click={fartted}>Fart</button>
+	<p>{fart}</p>
+</div>
