cd7bea45dc8104503885cb7966176ed936a47503
westin <westinp2009@gmail.com>
2024-03-10 21:34:09 -0400

    Add new components and dependencies

--- src/lib/character-card/character.card.svelte
+++ src/lib/character-card/character.card.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { CharacterClass } from '$lib/models/characterClass';
 	import { CharacterPhoto } from '$lib/models/characterPhoto';
+	import { IconPhoto } from '@tabler/icons-svelte';
 	/**
 	 * Character Card layout.
 	 * Photo with ability to change
@@ -25,3 +26,31 @@
 		10
 	);
 </script>
+
+<div>
+	<div class="char-Header">
+		{character.name}
+		{character.classType}
+	</div>
+	<div class="char-photo">
+		<!-- place holder -->
+		<IconPhoto />
+	</div>
+	<div class="char-stats">
+		{character.strength}
+		{character.strengthSavingThrow}
+		{character.dexterity}
+		{character.dexteritySavingThrow}
+		{character.constitution}
+		{character.constitutionSavingThrow}
+		{character.intelligence}
+		{character.intelligenceSavingThrow}
+		{character.wisdom}
+		{character.wisdomSavingThrow}
+		{character.charisma}
+		{character.charismaSavingThrow}
+	</div>
+</div>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,57 @@
-<script>
-	import Greet from '$lib/greet.svelte';
+<script lang="ts">
+	import Character from '$lib/character-card/character.card.svelte';
+	import { CharacterClass } from '$lib/models/characterClass';
+	import { CharacterPhoto } from '$lib/models/characterPhoto';
+
+	const characterList: Array<CharacterClass> = [];
+	let randAmount: number = Math.floor(Math.random() * 5) + 1;
+
+	for (let i = 0; i < randAmount; i++) {
+		addCharacter();
+	}
+
+	function addCharacter() {
+		characterList.push(
+			new CharacterClass(
+				'newName',
+				new CharacterPhoto('photo', 'photo', 'photo'),
+				'newClass',
+				10,
+				10,
+				10,
+				10,
+				10,
+				10
+			)
+		);
+	}
+
+	function rollInitiative() {
+		// call the roll initiative die roll lib in the future
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<Greet />
+<div class="d-flex">
+	<div class="title">
+		<h1>Goop Session Manger</h1>
+	</div>
+
+	<div>
+		<button on:click={rollInitiative}>roll initiative</button>
+	</div>
+	<br />
+
+	<button on:click={addCharacter}>Add Character</button>
+	<div class="char-row">
+		{#each characterList as character}
+			<Character {character} />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.char-row {
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
